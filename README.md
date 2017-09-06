d3b30508e57745c61100b17b7fb0841af7548017
Ebad Hashmi <ehashmi@etilizepak.com>
2017-09-06 11:18:57 +0500

    CAS-1 | Create project skeleton for Category Service | Ebad Hashmi
    - Added skeleton project

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Its for category-service
+Category service lets you create, read, update and delete categories for industry.
+
+# How to get the code
+
+Clone the git repository using the URL on the Git home page:
+
+    $ git clone http://git.etilizepak.com/burraq/category-service.git
+    $ cd category-service
+
+# Contributing to attribute-service
+
+Here are some ways for you to get involved:
+
+* Create [JIRA](http://jira.etilizepak.com/browse/CAS) tickets for bugs and new features and comment and vote on the ones that you are interested in.
+* If you want to write code, we encourage contributions through merge requests from forks of this repository. If you want to contribute code this way, please familiarize yourself with the process outlined for contributing to projects here: [Contributor Guidelines](http://git.etilizepak.com/automation/sde/wikis/Contributor-Guidelines).
---
+++ src/main/java/com/etilize/burraq/category/config/RestConfig.java
@@ -0,0 +1,105 @@
+/*
+ * #region
+ * category-service
+ * %%
+ * Copyright (C) 2017 Etilize
+ * %%
+ * NOTICE: All information contained herein is, and remains the property of ETILIZE.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * ETILIZE and may be covered by U.S. and Foreign Patents, patents in process, and
+ * are protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from ETILIZE. Access to the source code contained herein
+ * is hereby forbidden to anyone except current ETILIZE employees, managers or
+ * contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure of this source code, which includes information that is confidential
+ * and/or proprietary, and is a trade secret, of ETILIZE. ANY REPRODUCTION, MODIFICATION,
+ * DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS
+ * SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF ETILIZE IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT
+ * OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR
+ * IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO
+ * MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+ * #endregion
+ */
+
+package com.etilize.burraq.category.config;
+
+import javax.validation.Validator;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.rest.core.event.ValidatingRepositoryEventListener;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Houses all the rest related configurations
+ *
+ * @author Faisal Feroz
+ *
+ */
+@Configuration
+public class RestConfig extends RepositoryRestConfigurerAdapter {
+
+    /**
+     * Cors Filter for spring-data-rest and controllers
+     *
+     * @return corsFilter Object
+     */
+    @Order
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        config.setMaxAge(Long.MAX_VALUE);
+        config.addExposedHeader("Location");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    /**
+     * Overriding validations
+     *
+     * @param validatingListener Validating Listener
+     */
+    @Override
+    public void configureValidatingRepositoryEventListener(
+            final ValidatingRepositoryEventListener validatingListener) {
+        validatingListener.addValidator("beforeCreate", validator());
+        validatingListener.addValidator("beforeSave", validator());
+    }
+
+    /**
+     * creates {@link ValidatingMongoEventListener} configured with JSR-303
+     * {@link Validator}
+     *
+     * @return ValidatingMongoEventListener returns Validating Mongo Event Listener
+     */
+    @Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    /**
+     * creates JSR-303 {@link Validator}
+     *
+     * @return LocalValidatorFactoryBean return validator
+     */
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
+}
