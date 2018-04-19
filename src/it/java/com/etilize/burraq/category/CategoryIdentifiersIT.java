5b274c42ce4fb1a6f39c92a9d35d67145f2b81c2
ninam@etilizepak.com <admin@example.com>
2018-04-19 16:55:08 +0500

    CAS-47 Develop citrus cases for field identifiers in category model representing sorted set of attributeIds
    - Added case for a valid identifier
    
    - Added case for adding subset of identifiers of attributes
    
    - Added cases for adding invalid identifiers

---
+++ src/it/java/com/etilize/burraq/category/CategoryIdentifiersIT.java
@@ -0,0 +1,157 @@
+/*
+ * #region
+ * category-service
+ * %%
+ * Copyright (C) 2017 Etilize
+ * %%
+ * NOTICE: All information contained herein is, and remains the property of ETILIZE.
+ * The intellectual and technical concepts contained herein are proprietary to
+ * ETILIZE and may be covered by U.S. and Foreign Patents, patents in process, and
+ * are protected by trade secret or copyright law. Dissemination of this information
+ * or reproduction of this material is strictly forbidden unless prior written
+ * permission is obtained from ETILIZE. Access to the source code contained herein
+ * is hereby forbidden to anyone except current ETILIZE employees, managers or
+ * contractors who have executed Confidentiality and Non-disclosure agreements
+ * explicitly covering such access.
+ *
+ * The copyright notice above does not evidence any actual or intended publication
+ * or disclosure of this source code, which includes information that is confidential
+ * and/or proprietary, and is a trade secret, of ETILIZE. ANY REPRODUCTION, MODIFICATION,
+ * DISTRIBUTION, PUBLIC PERFORMANCE, OR PUBLIC DISPLAY OF OR THROUGH USE OF THIS
+ * SOURCE CODE WITHOUT THE EXPRESS WRITTEN CONSENT OF ETILIZE IS STRICTLY PROHIBITED,
+ * AND IN VIOLATION OF APPLICABLE LAWS AND INTERNATIONAL TREATIES. THE RECEIPT
+ * OR POSSESSION OF THIS SOURCE CODE AND/OR RELATED INFORMATION DOES NOT CONVEY OR
+ * IMPLY ANY RIGHTS TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO
+ * MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
+ * #endregion
+ */
+
+package com.etilize.burraq.category;
+
+import static com.etilize.burraq.category.config.Role.*;
+
+import org.junit.*;
+import org.springframework.http.*;
+
+import com.consol.citrus.annotations.*;
+import com.consol.citrus.context.*;
+import com.consol.citrus.message.*;
+import com.etilize.burraq.category.config.*;
+import com.etilize.burraq.category.test.base.*;
+
+/**
+ * This class contains test cases related Add Category functionality
+ *
+ * @author Nimra Inam
+ * @see AbstractIT
+ * @since 1.0.0
+ */
+public class CategoryIdentifiersIT extends AbstractIT {
+
+    public static final String EXISTING_CATEGORY_ID_TO_UPDATE = "5ad86b140fcdf819732fcbd6";
+
+    @Test
+    @CitrusTest
+    public void shouldAddCategoryIdentifier(@CitrusResource TestContext context)
+            throws Exception {
+        author("Nimra Inam");
+        description("A category identifier should be added");
+
+        final User user = props.getUserByRole(CREATE);
+        variable(USER_NAME_LABEL, user.getUsername());
+        applyBehavior(new AuthenticationBehavior(authenticationServiceClient, //
+                user.getUsername(), //
+                user.getPassword(), //
+                props.getClientId(), //
+                props.getClientSecret()));
+        String accessToken = context.getVariable("${accessToken}");
+
+        variable(LOCATION_HEADER_VALUE, "");
+        variable(CATEGORY_ID, EXISTING_CATEGORY_ID_TO_UPDATE);
+
+        putRequest(CATEGORY_URL, //
+                "${" + CATEGORY_ID + "}", //
+                readFile(
+                        "/datasets/categories/update/update_category_identifier_request.json"), //
+                accessToken);
+
+        extractHeader(HttpStatus.OK, HttpHeaders.LOCATION);
+        String categoryLocation = parseAndSetVariable(CATEGORY_URL,
+                context.getVariable("${locationHeaderValue}"));
+        verifyResponse(HttpStatus.OK, //
+                readFile(
+                        "/datasets/categories/update/update_category_identifier_response.json"), //
+                categoryLocation, //
+                accessToken);
+    }
+
+    @Test
+    @CitrusTest
+    public void shouldAddCategoryIdentifierAsSubsetOfAttributes(@CitrusResource TestContext context)
+            throws Exception {
+        author("Nimra Inam");
+        description("A category identifier should be added");
+
+        final User user = props.getUserByRole(CREATE);
+        variable(USER_NAME_LABEL, user.getUsername());
+        applyBehavior(new AuthenticationBehavior(authenticationServiceClient, //
+                user.getUsername(), //
+                user.getPassword(), //
+                props.getClientId(), //
+                props.getClientSecret()));
+        String accessToken = context.getVariable("${accessToken}");
+
+        variable(LOCATION_HEADER_VALUE, "");
+        variable(CATEGORY_ID, EXISTING_CATEGORY_ID_TO_UPDATE);
+
+        putRequest(CATEGORY_URL, //
+                "${" + CATEGORY_ID + "}", //
+                readFile(
+                        "/datasets/categories/update/update_category_identifier_subset_of_attributes_request.json"), //
+                accessToken);
+
+        extractHeader(HttpStatus.OK, HttpHeaders.LOCATION);
+        String categoryLocation = parseAndSetVariable(CATEGORY_URL,
+                context.getVariable("${locationHeaderValue}"));
+        verifyResponse(HttpStatus.OK, //
+                readFile(
+                        "/datasets/categories/update/update_category_identifier_subset_of_attributes_response.json"), //
+                categoryLocation, //
+                accessToken);
+    }
+
+    @Test
+    @CitrusTest
+    public void shouldAddInvalidCategoryIdentifier(@CitrusResource TestContext context)
+            throws Exception {
+        author("Nimra Inam");
+        description("An invalid category identifier should be added (it's a bug for now)");
+
+        final User user = props.getUserByRole(CREATE);
+        variable(USER_NAME_LABEL, user.getUsername());
+        applyBehavior(new AuthenticationBehavior(authenticationServiceClient, //
+                user.getUsername(), //
+                user.getPassword(), //
+                props.getClientId(), //
+                props.getClientSecret()));
+        String accessToken = context.getVariable("${accessToken}");
+
+        variable(LOCATION_HEADER_VALUE, "");
+        variable(CATEGORY_ID, EXISTING_CATEGORY_ID_TO_UPDATE);
+
+        putRequest(CATEGORY_URL, //
+                "${" + CATEGORY_ID + "}", //
+                readFile(
+                        "/datasets/categories/update/update_category_identifier_with_invalid_value_request.json"), //
+                accessToken);
+
+        extractHeader(HttpStatus.OK, HttpHeaders.LOCATION);
+        String categoryLocation = parseAndSetVariable(CATEGORY_URL,
+                context.getVariable("${locationHeaderValue}"));
+        verifyResponse(HttpStatus.OK, //
+                readFile(
+                        "/datasets/categories/update/update_category_identifier_with_invalid_value_response.json"), //
+                categoryLocation, //
+                accessToken);
+    }
+}
